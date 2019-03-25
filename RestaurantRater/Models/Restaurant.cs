18c7d79c11be60d10ba29da6da140d4b5ec5c2d2
wtravis923 <willie.travis@gmail.com>
2019-03-25 12:07:53 -0400

    added view

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace RestaurantRater.Models
 {
     public class Restaurant
     {
+        //RestaurantID will be the primary key; the identifier.
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+
+    // : is the inheritance symbol; if you get an error, you'll need to Ctrl . to add a using statement.
+    public class RestaurantDBContext : DbContext
+    {
+        //This sets a table that uses restaurant (class) as a guide. Columns for all properties. Will use first property as a primary key. 
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
