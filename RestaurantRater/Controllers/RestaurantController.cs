5bb98c4cc85c6eab3db69fa8a0d44e908412d4e8
wtravis923 <willie.travis@gmail.com>
2019-03-25 15:03:49 -0400

    Added Create Button and fields.

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
+        private RestaurantDBContext db = new RestaurantDBContext();
+
         // GET: Restaurant
         public ActionResult Index()
         {
+            return View(db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant); 
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+
         }
     }
 }
