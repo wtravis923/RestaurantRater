0dc12ebca8695a64ad32533082b45383674e7b16
wtravis923 <willie.travis@gmail.com>
2019-03-25 15:55:27 -0400

    Added Delete feature

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -30,13 +31,40 @@ namespace RestaurantRater.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Restaurants.Add(restaurant); 
+                db.Restaurants.Add(restaurant);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
             return View(restaurant);
 
+        }
+
+        // GET: Restaurant/Delete/id
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Delete/id
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restaurant restaurant = db.Restaurants.Find(id);
+            db.Restaurants.Remove(restaurant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
